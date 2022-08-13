998246681ddf84af0291b71a4f158b49af31d657
Brian Alegre <brialegre@yahoo.com>
2022-08-12 21:05:56 -0700

    Added Baseline Files\Folders.  Installed tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500">
+      Hello Project3!
+    </h1>
+  )
+}
+
+export default App;
