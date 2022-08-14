fcc35dd5d4f75937540d5cc866b6f55b720d3c4e
Axe714 <axearzadon@gmail.com>
2022-08-13 23:05:58 -0700

    Set-up the client folder.. verified functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,22 @@
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
 
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-green-500">
-      Hello Project3!
-    </h1>
-  )
-}
-
-
+const App = () => (
+  <ApolloProvider client={client}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </ApolloProvider>
+);
 
 export default App;
-
-
-// USE FOR LATER
-//
-
-// import React from 'react';
-// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import SingleThought from './pages/SingleThought';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <Router>
-//         <div className="flex-column justify-flex-start min-100-vh">
-//           <Header />
-//           <div className="container">
-//             <Routes>
-//               <Route
-//                 path="/"
-//                 element={<Home />}
-//               />
-//               </Routes >
-//               </div >
-//   <Footer />
-//             </div >
-//           </Router >
-//         </ApolloProvider >
-//       );
-//     }
-// export default App;
-//
---
+++ client/src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <span className="text-5xl text-red-800">Hey there!</span>
+  )
+}
