c841c6c86e3963fde8d16441bd2814c6e212dd6a
keeezy <lazarokev@gmail.com>
2022-08-14 21:20:45 -0700

    ServiceCategory model

---
+++ server/models/ServiceCategory.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const serviceType = require('./ServiceType')
+
+const serviceCategorySchema = new Schema ({
+    categoryName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    serviceType: [ServiceType.schema]
+});
+
+const ServiceCategory = mongoose.model('ServiceCategory', serviceCategorySchema);
+
+module.exports = ServiceCategory
