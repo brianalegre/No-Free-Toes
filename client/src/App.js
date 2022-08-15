3f5445b37714e55aa8da480259d1f23c4e502847
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-08-14 20:25:06 -0700

    added login/signup path for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -14,6 +16,8 @@ const App = () => (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   </ApolloProvider>
