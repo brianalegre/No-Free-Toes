2b52e231226bf7454d11dea31f5d1483d4224e3f
Brian Alegre <brialegre@yahoo.com>
2022-08-16 18:26:53 -0700

    Created baseline CategoryPage component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+import SingleServicePage from "./components/ServiceType/ServiceType";
+import CategoryPage from "./components/Category/Category";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -14,6 +16,8 @@ const App = () => (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/category/" element={<CategoryPage />} />
+        <Route path="/category/:servicetype" element={<SingleServicePage />} />
       </Routes>
     </Router>
   </ApolloProvider>
--- client/src/components/Login/pages/ClientLogin.js
+++ client/src/components/Login/pages/ClientLogin.js
@@ -29,7 +29,7 @@ return (
         </div>
     </form>
 </div>
-);n
+);
 };
 
 export default ClientLogin;
