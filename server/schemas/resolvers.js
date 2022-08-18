f87c356ddf25f61c6b81640f39e354c148bb6f92
Brian Alegre <brialegre@yahoo.com>
2022-08-18 09:10:58 -0700

    Client - added GlobalState and Queries Files.  Server, updating resolvers for Queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,24 @@ const { NormalUser,
     ServiceComment,
     ServiceType } = require('../models');
 
-const {signToken} = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
 
 
+const resolvers = {
+    Query: {
+        // SINGLE USER
+        me: async () => (user, args) => {
+            return User.findOne({
+                $or: [
+                    { _id: user ? user._id : args.id },
+                    { username: args.username }
+                ],
+            })
+        }
+    }
+}
+
+
+module.exports = resolvers;
