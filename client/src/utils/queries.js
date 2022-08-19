58732679f123dc808429b8b50fe9fa366fff1ccf
Brian Alegre <brialegre@yahoo.com>
2022-08-19 15:50:05 -0700

    Debugging issues with getting data back from the server

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,52 +1,54 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ALL_NORMALUSERS = gql`
-    {
-      normalUsers {
-      _id
-      firstName
-      lastName
-      email
-    }
-  }
-`;
-
-
-export const serviceUsersCategory = gql`
-  query getServiceUsersCategory($category: ID) {
-    serviceUsers(category: $category) {
-      _id
-      firstName
-      lastName
-      category {
-        _id
-      }
-    }
-  }
-`;
-
-export const QUERY_ALL_SERVICEUSERS = gql`
-    {
-      serviceUsers {
-      _id
-      firstName
-      lastName
-      email
-      serviceCategory {
-        _id
-        categoryName
-        categoryIcon
-      }
-    }
-    }
-`
 
 export const QUERY_ALL_SERVICECATEGORIES = gql`
-query ServiceCategories {
-  serviceCategories {
-    _id
-    categoryName
-    categoryIcon
+    query serviceCategories {
+      serviceCategories { 
+      _id
+      categoryName
+      categoryIcon
+    }
   }
-}
-`
+`;
+
+// export const QUERY_ALL_NORMALUSERS = gql`
+//     {
+//       normalUsers {
+//       _id
+//       firstName
+//       lastName
+//       email
+//     }
+//   }
+// `;
+
+
+// export const serviceUsersCategory = gql`
+//   query getServiceUsersCategory($category: ID) {
+//     serviceUsers(category: $category) {
+//       _id
+//       firstName
+//       lastName
+//       category {
+//         _id
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_ALL_SERVICEUSERS = gql`
+//     {
+//       serviceUsers {
+//       _id
+//       firstName
+//       lastName
+//       email
+//       serviceCategory {
+//         _id
+//         categoryName
+//         categoryIcon
+//       }
+//     }
+//     }
+// `
+
