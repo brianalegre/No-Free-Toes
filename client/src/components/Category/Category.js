2bcbf77ddfcfbbe38f51ce8b3095b9a6041d1733
Brian Alegre <brialegre@yahoo.com>
2022-08-20 12:00:21 -0700

    Added styling to serviceUser-card

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -48,11 +48,10 @@ function CategoryPage() {
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
               </div>
             </div>
-            <div className="flex flex-col justify-between p-4 leading-normal">
+            <div className="flex flex-col justify-between p-4 leading-normal w-96">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{serviceUser.firstName} {serviceUser.lastName}</h5>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{serviceUser.bio}</p>
             </div>
-            {/* <button type="button" className="text-black bg-green-300 hover:bg-green-700 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-300 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-200">View Profile</button> */}
             <Link to="/service/test1"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-3 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View Profile</button></Link>
           </div>
         </div>
