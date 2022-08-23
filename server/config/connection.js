6ac810f1ef4ec1806cc2168b1bdb92413e077e4d
Brian Alegre <brialegre@yahoo.com>
2022-08-23 13:53:02 -0700

    Added Notes to connection.js about seeding Mongodb Atlas vs Mongodb Local

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+
+// UNCOMMENT OUT TO CONNECT TO MONGODB ATLAS
+// LEAVE COMMENTED TO CONNECT LOCALLY
+// NPM RUN SEED
+// COMMAND IS USED TO SEED LOCALLY OR MONGODB, DEPENDS ON
+// WHAT IS COMMENTED OUT
 require('dotenv').config()
 
-console.log(process.env.MONGODB_URI)
-console.log(process.env.MONGODB_URI)
+// console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/noFreeToes', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
