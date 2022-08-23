d25772606efcb9a78fc655382f94d6bbcec24174
Axe714 <axearzadon@gmail.com>
2022-08-23 15:37:23 -0700

    Adding basic styling to review form subcomponent

--- client/src/components/ServicePage/subcomponents/ReviewForm.js
+++ client/src/components/ServicePage/subcomponents/ReviewForm.js
@@ -1,22 +1,46 @@
 import React from "react";
+import { DynamicStar } from "./StarRating";
 
 export default function ReviewForm() {
   return (
     <>
+      <div className="-mx-6 sm:-mx-8 px-4 sm:px-6 bg-gray-100">
+        <div className="inline-block min-w-full"></div>
+      </div>
+
+      <div className="-mx-6 sm:-mx-8 px-4 sm:px-7 bg-gray-100">
+        <div className="flex flex-col min-w-full pl-2">
+          <span className="text-sm sm:text-base">
+            Service Provider Rating:{" "}
+          </span>
+          <DynamicStar />
+        </div>
+      </div>
+
+      <div className="-mx-6 sm:-mx-8 px-4 sm:px-6 sm:-mb-3 bg-gray-100">
+        <div className="inline-block min-w-full"></div>
+      </div>
+
       <textarea
         id="message"
         rows="4"
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-0"
-        placeholder="Leave a comment..."
-      ></textarea>
+        className="block p-2.5 w-full text-sm sm:text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+        placeholder="Leave a review..."
+      />
+
       <div className="-mx-6 sm:-mx-8 px-4 sm:px-6 bg-gray-100">
         <div className="inline-block min-w-full"></div>
+
         <div className="flex justify-end">
           <button
+            onClick={() => {
+              window.alert("You left a review");
+              window.location.reload();
+            }}
             type="button"
-            className="text-white bg-green-400 hover:bg-green-800 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700"
+            className="text-white bg-green-600 hover:bg-green-800 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2"
           >
-            Submit
+            Submit Review
           </button>
         </div>
       </div>
