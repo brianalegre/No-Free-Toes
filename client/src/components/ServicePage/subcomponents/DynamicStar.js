66783128d10d1246e123903a44d5c516a3162a3f
Axe714 <axearzadon@gmail.com>
2022-08-24 18:02:07 -0700

    Modified dynamic star subcomponent

--- client/src/components/ServicePage/subcomponents/DynamicStar.js
+++ client/src/components/ServicePage/subcomponents/DynamicStar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 
 const dynamicStar = (
@@ -13,47 +14,93 @@ const dynamicStar = (
 );
 
 export default function DynamicStar({ starRef }) {
-  // export default function DynamicStar() {
+  // set the initial state of 5 items to 'null' value
+  // 5 nulls are just place holders to specify that we have 5 items in this hook
+  const [clickedStars, setClickedStars] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  // create a function that sets the stars state to 'clickedStars' on click
+  // for loop to set the state of the clicked star and all indexes below it to true
+  // and everything else above the clicked index to false
+  const handleStarClick = (e, index) => {
+    e.preventDefault();
+    let clickStates = [...clickedStars];
+    for (let i = 0; i < 5; i++) {
+      i <= index ? (clickStates[i] = true) : (clickStates[i] = false);
+    }
+    setClickedStars(clickStates);
+  };
+
   return (
     <div className="flex">
       <div
         onClick={(e) => {
           e.stopPropagation();
+          handleStarClick(e, 0);
           starRef.current = 1;
         }}
-        className="flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+        className={
+          clickedStars[0]
+            ? "flex text-yellow-400"
+            : "flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+        }
       >
         {dynamicStar}
         <div
           onClick={(e) => {
             e.stopPropagation();
+            handleStarClick(e, 1);
             starRef.current = 2;
           }}
-          className="flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+          className={
+            clickedStars[1]
+              ? "flex text-yellow-400"
+              : "flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+          }
         >
           {dynamicStar}
           <div
             onClick={(e) => {
               e.stopPropagation();
+              handleStarClick(e, 2);
               starRef.current = 3;
             }}
-            className="flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+            className={
+              clickedStars[2]
+                ? "flex text-yellow-400"
+                : "flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+            }
           >
             {dynamicStar}
             <div
               onClick={(e) => {
                 e.stopPropagation();
+                handleStarClick(e, 3);
                 starRef.current = 4;
               }}
-              className="flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+              className={
+                clickedStars[3]
+                  ? "flex text-yellow-400"
+                  : "flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+              }
             >
               {dynamicStar}
               <div
                 onClick={(e) => {
                   e.stopPropagation();
+                  handleStarClick(e, 4);
                   starRef.current = 5;
                 }}
-                className="flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+                className={
+                  clickedStars[4]
+                    ? "flex text-yellow-400"
+                    : "flex text-gray-400 transition ease-in-out hover:text-yellow-400"
+                }
               >
                 {dynamicStar}
               </div>
