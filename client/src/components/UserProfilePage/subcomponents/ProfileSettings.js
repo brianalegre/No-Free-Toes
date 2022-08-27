425eff597e0f4af66de298630e4f2f81af9026ea
keeezy <lazarokev@gmail.com>
2022-08-26 23:23:21 -0700

    fixed email debugg

--- client/src/components/UserProfilePage/subcomponents/ProfileSettings.js
+++ client/src/components/UserProfilePage/subcomponents/ProfileSettings.js
@@ -9,7 +9,7 @@ export default function ProfileSettings({ loggedInUserId, email, firstName, last
     // CREATE STATE FROM FORM
     const [userInfo, setUserInfo] = useState({
         normalUserId: loggedInUserId,
-        email: '',
+        email: email,
         firstName: '',
         lastName: '',
         password: '',
@@ -62,7 +62,7 @@ export default function ProfileSettings({ loggedInUserId, email, firstName, last
                 </h1>
                 <div className="relative z-0 mb-6 w-full group">
                     <label for="floating_email" className="text-xs">Email address</label>
-                    <input onChange={handleUserInput} type="email" name="email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder={email} required />
+                    <input onChange={handleUserInput} defaultValue={email} type="email" name="email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
                 </div>
                 <div className="relative z-0 mb-6 w-full group">
                     <input onChange={handleUserInput} type="password" name="password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
