a8f7c51df40572e16cdf1626f3296a11f68cba98
Axe714 <axearzadon@gmail.com>
2022-09-01 13:07:31 -0700

    Added font family, simple homepage animations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nato': ['Noto Sans', 'sans-serif'],
+        'kanit': ['Kanit', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
