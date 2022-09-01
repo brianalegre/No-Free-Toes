ccd1b557c1edf8ab0d9dfde529b4e59d40450320
Brian Alegre <brialegre@yahoo.com>
2022-09-01 14:46:52 -0700

    Fixed errors in TimeSlotSettings.js

--- client/src/components/ServiceAccount/subcomponents/TimeSlotSettings.js
+++ client/src/components/ServiceAccount/subcomponents/TimeSlotSettings.js
@@ -58,9 +58,9 @@ export default function TimeSlotSettings(serviceUser) {
             className="flex-shrink-0 h-6 w-6 -ml-1 mr-2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M12 6v6m0 0v6m0-6h6m-6 0H6"
             />
           </svg>
