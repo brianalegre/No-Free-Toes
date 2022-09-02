dc9c88953aae38a1a8e9a8dd49c53ca24626dd9d
Brian Alegre <brialegre@yahoo.com>
2022-09-02 10:56:36 -0700

    Fixed errors seen in appointementCards

--- client/src/components/Appointment/subcomponents/SUAppointmentCards.js
+++ client/src/components/Appointment/subcomponents/SUAppointmentCards.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_SERVICEUSER } from "../../../utils/queries";
@@ -17,7 +17,7 @@ export default function SUAppointmentCards() {
 
   const [deleteAppointment] = useMutation(DELETE_APPOINTMENT);
 
-  useEffect(()=> {
+  useEffect(() => {
     refetch()
   }, [data, refetch])
 
@@ -34,9 +34,9 @@ export default function SUAppointmentCards() {
           },
         });
 
-       toast.success("Appointment successfully deleted")
-       refetch()
-       return Reoverlay.hideModal()
+        toast.success("Appointment successfully deleted")
+        refetch()
+        return Reoverlay.hideModal()
       }
     })
   }
@@ -54,7 +54,7 @@ export default function SUAppointmentCards() {
         />
         <div className="absolute top-2 -right-0.5 md:bottom-40">
           <button
-            onClick={()=> deleteApptHandler(appt._id, appt.normalUser._id)}
+            onClick={() => deleteApptHandler(appt._id, appt.normalUser._id)}
             className="inline-flex items-center justify-center w-5 h-5 mr-2 text-pink-100 transition delay-50 ease-in-out bg-red-500 rounded-lg focus:shadow-outline hover:bg-red-700"
           >
             <svg
@@ -65,9 +65,9 @@ export default function SUAppointmentCards() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
               />
             </svg>
