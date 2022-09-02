5b2e9306c0cf9b1218f1787f7b10bb8e27425b0d
Brian Alegre <brialegre@yahoo.com>
2022-09-02 10:48:47 -0700

    Changed text when user has no reviews

--- client/src/components/UserAccount/subcomponents/NoProfileReviews.js
+++ client/src/components/UserAccount/subcomponents/NoProfileReviews.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NoProfileReviews() {
   return (
     <div className="flex shadow-md justify-center max-w-lg pl-3 sm:pl-4 py-4 m-4 rounded-lg text-gray-900 dark:border-gray-700 dark:bg-gray-800 dark:text-white">
-        <span className="text-lg font-bold">No reviews posted.</span>
+      <span className="text-lg font-bold">You haven't posted any reviews...</span>
     </div>
   )
 }
