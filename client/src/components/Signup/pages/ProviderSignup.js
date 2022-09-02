66923a21272be1af0a26a356920da680d198a34d
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-09-01 23:13:24 -0700

    pulling

--- client/src/components/Signup/pages/ProviderSignup.js
+++ client/src/components/Signup/pages/ProviderSignup.js
@@ -102,7 +102,7 @@ const ProviderSignup = () => {
         </div>
       </div>
       <main className="p-5 min-w-center m-auto w-full text-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 bg-gray-800 border-gray-700">
-        <div className="p-2 col-12 col-lg-10">
+        <div className="p-a col-12 col-lg-10">
           <div className="card">
             <h4 className="mb-4 text-3xl font-bold text-white">Provider Sign Up</h4>
             <h5 className="mb-4 text-sm font-bold text-white">Please fill out the form below:</h5>
